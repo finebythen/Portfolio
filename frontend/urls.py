cef44ed6c3aea3fe26675ddc1359d58f5ceb00f2
Finn Then <71402477+finebythen@users.noreply.github.com>
2022-01-12 21:22:19 +0100

    login & logout

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 
 urlpatterns = [
+    # login & logout
+    path('login/', views.loginPage, name="login"),
+	path('logout/', views.logoutUser, name="logout"),
+
+    # templates
     path('', views.frontend_main, name="frontend-main"),
     path('languages/', views.frontend_languages, name="frontend-languages"),
     path('resume/', views.frontend_resume, name="frontend-resume"),
--- frontend/views.py
+++ frontend/views.py
@@ -1,11 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .decorators import unauthenticated_user, allowed_users
+
+
+# Lists for decorator '@allowed_users'
+ALLOWED_ALL = [
+	'Administrator',
+]
+
+ALLOWED_RESTRICTED = [
+	'Administrator',
+]
+
+ALLOWED_ADMIN = [
+	'Administrator',
+]
 
 # Create your views here.
+@unauthenticated_user
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('main')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('frontend-main')
+			else:
+				messages.info(request, 'Benutzername oder Passwort stimmen nicht überein!')
+
+		return render(request, 'frontend/login.html')
+
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=ALLOWED_ALL)
 def frontend_main(request):
     return render(request, 'frontend/main.html')
 
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=ALLOWED_ALL)
 def frontend_languages(request):
     return render(request, 'frontend/languages.html')
 
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=ALLOWED_ALL)
 def frontend_resume(request):
     return render(request, 'frontend/resume.html')
